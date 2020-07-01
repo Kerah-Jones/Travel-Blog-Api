64ad2b4b88e8cbddbd0726e426fd718c971799e6
Kerah-Jones <Kerahjones@gmail.com>
2020-07-01 14:28:03 -0400

    Fixed a few routes in blog routes that were not responding to the front end application.

--- app/routes/blog_routes.js
+++ app/routes/blog_routes.js
@@ -1,22 +1,29 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
+const requireToken = passport.authenticate('bearer', { session: false })
+const removeBlanks = require('../../lib/remove_blank_fields')
 
 // require restaurant model
 const Blog = require('../models/blog')
 const handle404 = require('../../lib/custom_errors')
 
 // INDEX
-// GET /restaurants
-router.get('/blogs', (req, res, next) => {
+// GET /blogs
+router.get('/blogs', requireToken, (req, res, next) => {
   Blog.find()
-    .populate('owner')
-    .populate('reviews.reviewer')
+    .then(examples => {
+    // `examples` will be an array of Mongoose documents
+    // we want to convert each one to a POJO, so we use `.map` to
+    // apply `.toObject` to each one
+      return examples.map(example => example.toObject())
+    })
     .then(blogs => res.json({ blogs: blogs }))
     .catch(next)
 })
 
 // SHOW
-// GET /restaurants/:id
+// GET /blogs/:id
 router.get('/blogs/:id', (req, res, next) => {
   const id = req.params.id
   Blog.findById(id)
@@ -28,28 +35,36 @@ router.get('/blogs/:id', (req, res, next) => {
 })
 
 // CREATE
-// POST /restaurants/
-router.post('/blogs', (req, res, next) => {
-  const blogData = req.body.blog
-  Blog.create(blogData)
-    .then(blog => res.status(201).json({blog: blog}))
+// POST /blogs/
+router.post('/blogs', requireToken, (req, res, next) => {
+  // set owner of new example to be current user
+  req.body.blog.owner = req.user.id
+
+  Blog.create(req.body.blog)
+    // respond to succesful `create` with status 201 and JSON of new "example"
+    .then(blog => {
+      res.status(201).json({ blog: blog.toObject() })
+    })
+    // if an error occurs, pass it off to our error handler
+    // the error handler needs the error message and the `res` object so that it
+    // can send an error message back to the client
     .catch(next)
 })
 
 // UPDATE
-// PATCH /restaurants/:id
-router.patch('/blogs/:id', (req, res, next) => {
+// PATCH /blogs/:id
+router.patch('/blogs/:id', requireToken, removeBlanks, (req, res, next) => {
   const id = req.params.id
   const blogData = req.body.blog
   Blog.findById(id)
     .then(handle404)
-    .then(blog => blog.update(blogData))
+    .then(blog => blog.updateOne(blogData))
     .then(() => res.sendStatus(204))
     .catch(next)
 })
 
 // DESTROY
-// DELETE /restaurants/:id
+// DELETE /blogs/:id
 router.delete('/blogs/:id', (req, res, next) => {
   const id = req.params.id
   Blog.findById(id)
--- curl-scripts/blog/update.sh
+++ curl-scripts/blog/update.sh
@@ -1,9 +1,10 @@
 # sh curl-scripts/index.sh
 
-curl "http://localhost/4741/blogs/${ID}" \
---include \
---request PATCH \
---header "Content-Type: application/json" \
+curl "http://localhost:4741/blogs/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+--header "Authorization: Bearer ${TOKEN}" \
 --data '{
   "blog": {
     "title": "'"${TITLE}"'",
