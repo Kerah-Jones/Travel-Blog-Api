8df9ea967678899c27ed82571124d8afab9b5579
Kerah-Jones <Kerahjones@gmail.com>
2020-07-13 18:15:18 -0400

    Create review added and working

--- app/routes/reviewRoutes.js
+++ app/routes/reviewRoutes.js
@@ -6,28 +6,29 @@ const removeBlanks = require('../../lib/remove_blank_fields')
 const customErrors = require('../../lib/custom_errors')
 const requireOwnership = customErrors.requireOwnership
 
-// require restaurant model
+// require blog model
 const Blog = require('../models/blog')
 const handle404 = require('../../lib/custom_errors')
 
 // CREATE
 // POST /reviews/
-router.post('/reviews', requireToken, (req, res, next) => {
+router.post('/reviews/:blogId', requireToken, (req, res, next) => {
   // get the review data from the body of the request
+  req.body.review.owner = req.user.id
   const reviewData = req.body.review
   // get the blog id from the body
-  const blogId = reviewData.blogId
-  // find the restaurant by its id
+  const blogId = req.params.blogId
+  // find the blog by its id
   Blog.findById(blogId)
     .then(handle404)
     .then(blog => {
-      // add review to restaurant
+      // add review to blog
       blog.reviews.push(reviewData)
-      // save restaurant
+      // save blog
       return blog.save()
     })
     // send responsne back to client
-    .then(blog => res.status(201).json({restaurant: blog}))
+    .then(blog => res.status(201).json({blog: blog}))
     .catch(next)
 })
 module.exports = router
